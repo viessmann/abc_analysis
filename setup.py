52f1740bca5bb2e2d4a9cb9e6de788fcd369c8ff
Tino_Gehlert <ghlt@viessmann.net>
2019-03-08 08:34:31 +0100

    version incremented to 0.1.1

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ def readme():
 
 
 setup(name='abc_analysis',
-      version='0.1.0',
+      version='0.1.1',
       description='ABC analysis with automated limit detection',
       long_description=readme(),
-      download_url='https://github.com/viessmann/abc_analysis/archive/v0.1.0.tar.gz',
+      download_url='https://github.com/viessmann/abc_analysis/archive/v0.1.1.tar.gz',
       classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
