21c1b5ebc08a8f9e8c356d394afc2073765eff0a
Tino_Gehlert <ghlt@viessmann.net>
2019-03-08 09:35:36 +0100

    long description added

--- abc_analysis/__init__.py
+++ abc_analysis/__init__.py
@@ -1,3 +1,3 @@
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 from .abc_analysis import abc_analysis, abc_plot
--- setup.py
+++ setup.py
@@ -1,20 +1,33 @@
 from setuptools import setup, find_packages
 
-def readme():
-    with open('README.md') as f:
-        return f.read()
-
 
 setup(name='abc_analysis',
-      version='0.1.2',
+      version='0.1.3',
       description='ABC analysis with automated limit detection',
-      long_description=readme(),
-      download_url='https://github.com/viessmann/abc_analysis/archive/v0.1.2.tar.gz',
+      long_description="""
+      Performs and visualizes an ABC analysis. This package is a Python 
+      implementation of the R package 
+      `ABCanalysis <https://CRAN.R-project.org/package=ABCanalysis>`__
+      
+      **Basic Usage**
+      
+      .. code-block:: python
+          from abc_analysis import abc_analysis, abc_plot
+          
+          # Perform an ABC analysis on a numeric vector (without plotting)
+          dctAnalysis = abc_analysis([1, 15, 25, 17, 2, 3, 5, 6, 2, 3, 22])
+          
+          # Perform an ABC analysis with plotting
+          dctAnalysis = abc_analysis([1, 15, 25, 17, 2, 3, 5, 6, 2, 3, 22], True)
+          
+          # Plot saved results of an ABC analysis
+          abc_plot(dctAnalysis)
+      """,
+      download_url='https://github.com/viessmann/abc_analysis/archive/v0.1.3.tar.gz',
       classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3.6',
-        'Topic :: Software Development :: Libraries :: Python Modules',
         'Topic :: Scientific/Engineering',
       ],
       keywords='abc-analysis abc_analysis',
