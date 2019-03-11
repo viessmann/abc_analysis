63072777a03838d9a0ce11a45c31e8bdc1554e48
Tino_Gehlert <ghlt@viessmann.net>
2019-03-11 11:06:46 +0100

    keywords and project_urls (unfortunately not rendered in PyPi) added

--- setup.py
+++ setup.py
@@ -2,21 +2,22 @@ from setuptools import setup, find_packages
 
 
 setup(name='abc_analysis',
-      version='0.1.8',
+      version='0.1.9',
+      project_urls={'Viessmann': 'https://www.linkedin.com/company/viessmann/'},
       description='ABC analysis with automated limit detection',
       long_description="""
 Performs and visualizes an ABC analysis with automated limit detection.
 
 This package is a Python implementation of the R package `ABCanalysis <https://CRAN.R-project.org/package=ABCanalysis>`__
 """,
-      download_url='https://github.com/viessmann/abc_analysis/archive/v0.1.8.tar.gz',
+      download_url='https://github.com/viessmann/abc_analysis/archive/v0.1.9.tar.gz',
       classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3.6',
         'Topic :: Scientific/Engineering',
       ],
-      keywords='abc-analysis abc_analysis',
+      keywords='abc-analysis abc_analysis Viessmann',
       url='https://github.com/viessmann/abc_analysis',
       author='Tino Gehlert',
       author_email='ghlt@viessmann.com',
